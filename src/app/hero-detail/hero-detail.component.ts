7456a8989524f23ad936f2d927725b3ff85f893c
Ryan Mossor <ryan.mossor@gmail.com>
2023-01-31 11:11:02 -0600

    Completed lesson 5 - routing

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 
 @Component({
@@ -9,4 +13,27 @@ import { Hero } from '../hero';
 export class HeroDetailComponent {
   // @Input decorator necessary to receive data from parent component via 1-way property binding
   @Input() hero?: Hero;
+
+  constructor(
+    private route: ActivatedRoute, // holds info about the route (e.g., route params) to this instance of the component
+    private heroService: HeroService, // gets hero data from remote server; component displays this data
+    private location: Location // Angular service for interacting w/ browser; lets you navigate to previous view
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    // route.snapshot: static image of the route info shortly after component was created
+    // paramMap: dictionary of route param values extracted from URL; 'id' key returns ID of the hero to fetch
+    // Number() used since route params are always strings, and we want hero's ID to be a number
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
